4979970213951df2c8d943561a9f2722de35a18e
mic0608 <46119749+mic0608@users.noreply.github.com>
2021-08-21 14:24:07 +0800

    Update life.py

--- life.py
+++ life.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 df = pd.DataFrame({'lob':list('qwerasdf),'life':list('12345678')})
 spark.createDataFrame(df).write.mode('overwrite').parquet('/FileStore/table/life')
+
+for i in range(10000000):
+  a=i
