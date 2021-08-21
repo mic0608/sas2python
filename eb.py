0034905813d973cd0e11ded64f3445b831d71c45
mic0608 <46119749+mic0608@users.noreply.github.com>
2021-08-21 14:24:20 +0800

    Update eb.py

--- eb.py
+++ eb.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 df = pd.DataFrame({'lob':list('qwerasdf),'eb':list('12345678')})
 spark.createDataFrame(df).write.mode('overwrite').parquet('/FileStore/table/eb')
+
+                              
+for i in range(10000000):
+  a=i
